52a1a6ae492342f68a1cc1693f8132d98790b7f6
FelixLP <85683853+felixlp938@users.noreply.github.com>
2023-01-07 21:47:47 +0100

    [NOT TESTED!] modifyed systemfiles (Project files, and nupkg files)

--- obj/Debug/net7.0/CodeTagebuch.AssemblyInfo.cs
+++ obj/Debug/net7.0/CodeTagebuch.AssemblyInfo.cs
@@ -11,8 +11,9 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CodeTagebuch")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("FelixLP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("(c) 2023 FelixLP")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeTagebuch")]
