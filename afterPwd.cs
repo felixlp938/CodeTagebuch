e94405aa11a7103882bb72d587e0a44d8f256d01
FelixLP <85683853+felixlp938@users.noreply.github.com>
2023-01-13 16:46:02 +0100

    formated the file afterPwd.cs

--- afterPwd.cs
+++ afterPwd.cs
@@ -17,231 +17,231 @@ namespace afterPwd
 			loggedin.perfomCommand(Console.ReadLine());
 		}
 
-			public void perfomCommand(string? COMMAND)
-			{
-				if (COMMAND is null)
-				{
-					return;
-				}
-
-				switch (COMMAND.ToLower())
-				{
-					case "read":
-						Console.WriteLine("\n\nEntering read mode!\n");
-						FileMgmt.readMode();
-						break;
-
-					case "write":
-						Console.WriteLine("\n\nEntering write mode!\n");
-						break;
-
-					case "delete":
-						Console.WriteLine("\n\nEntering delete mode!\n");
-						break;
-
-					case "modify":
-						Console.WriteLine("\n\nEntering modify mode!\n");
-						break;
-
-					case "exit":
-						Environment.Exit(0);
-						break;
-
-					case "logout":
-						Console.Clear();
-						application.Runnable.Main();
-						break;
-
-					case "settings":
-						Console.WriteLine("\n\nEntering settings mode!\n");
-						CodeTagebuch.Settings settings = new CodeTagebuch.Settings();
-						settings.acces(USERNAME);
-						break;
-
-					default:
-						Console.Clear();
-						init();
-						break;
-				}
-			}
-		}
-
-		public class FileMgmt
+		public void perfomCommand(string? COMMAND)
 		{
-			public static void readMode()
+			if (COMMAND is null)
 			{
-				Console.Write("Dateinamen zum öffnen eingeben: ");
-				string? filename = Console.ReadLine();
-
-				if (File.Exists(filename))
-				{
-					Console.WriteLine(File.ReadAllLines(filename));
-				}
-				else
-				{
-					Console.WriteLine("Das hat leider nicht geklappt! Existiert die Datei?");
-				}
-
-				Console.Clear();
-				afterPwd.loggedIn.init();
+				return;
 			}
-			public static void writeMode()
+
+			switch (COMMAND.ToLower())
 			{
-				Console.Write("Dateinamen zum öffnen eingeben: ");
-				string? filename = Console.ReadLine();
-				Console.Write("\nSoll etwas an die Datei ANGEÄNGT werden? [ TRUE | FALSE ] ");
-				string? append = Console.ReadLine();
+				case "read":
+					Console.WriteLine("\n\nEntering read mode!\n");
+					FileMgmt.readMode();
+					break;
 
-				bool append_bool = false;
-			
-				if (append != null)
-				{
-					if (append.ToLower() == "true")
-					{
-						append_bool = true;
-					}
-					else if (append.ToLower() == "false")
-					{
-						append_bool = false;
-					}
-				}
+				case "write":
+					Console.WriteLine("\n\nEntering write mode!\n");
+					break;
 
-				if (File.Exists(filename))
-				{
-					StreamWriter write = new StreamWriter(filename, append_bool);
+				case "delete":
+					Console.WriteLine("\n\nEntering delete mode!\n");
+					break;
 
+				case "modify":
+					Console.WriteLine("\n\nEntering modify mode!\n");
+					break;
 
-					write.WriteLine(Console.ReadLine());
+				case "exit":
+					Environment.Exit(0);
+					break;
 
-					write.Close();
+				case "logout":
+					Console.Clear();
+					application.Runnable.Main();
+					break;
 
-				}
-				else
-				{
-					Console.WriteLine("Das hat leider nicht geklappt! Ist die Datei vorhanden und hat keinen Schreibschutz?");
-				}
+				case "settings":
+					Console.WriteLine("\n\nEntering settings mode!\n");
+					CodeTagebuch.Settings settings = new CodeTagebuch.Settings();
+					settings.acces(USERNAME);
+					break;
 
-				Console.Clear();
-				afterPwd.loggedIn.init();
+				default:
+					Console.Clear();
+					init();
+					break;
 			}
 		}
 	}
 
-	public class secManager
+	public class FileMgmt
 	{
-		public void init()
+		public static void readMode()
 		{
-			if (!File.Exists("default.pwd"))
+			Console.Write("Dateinamen zum öffnen eingeben: ");
+			string? filename = Console.ReadLine();
+
+			if (File.Exists(filename))
 			{
-				try
-				{
-					StreamWriter writer = new StreamWriter("default.pwd");
-					writer.WriteLine("USERNAME=default");
-					writer.WriteLine("PWD=");
-					writer.WriteLine("SETTINGS=true");
-					writer.WriteLine("MOTD=Das ist ein default Account!");
-					writer.Close();
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine($"Das erstellen des DeafultUsers hat leider nicht geklappt! Error: {e.Message}");
-				}
-			}
-		}
-
-		public bool chkPwd(string? USERNAME, string? PASSWORD)
-		{
-
-			if (USERNAME is null || PASSWORD is null)
-			{
-				return false;
-			}
-
-
-			if (File.Exists(USERNAME + ".pwd"))
-			{
-				try
-				{
-					bool usernameCorrect = false;		//username check for the current user
-					bool pwdCorrect = false;			//pwd check for the current user
-
-					foreach (string line in File.ReadLines(USERNAME + ".pwd"))
-					{
-						if (line.StartsWith("USERNAME="))
-						{
-							if (line.Substring(9) == USERNAME)
-							{
-								usernameCorrect = true;
-							}
-							else
-							{
-								usernameCorrect = false;
-							}
-						}
-						else if (line.StartsWith("PWD="))
-						{
-							if (line.Substring(4) == PASSWORD)
-							{
-								pwdCorrect = true;
-							}
-							else
-							{
-								pwdCorrect = false;
-							}
-						}
-					}
-
-					if (usernameCorrect == true && pwdCorrect == true)
-					{
-						return true;
-					}
-					else
-					{
-						return false;
-					}
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine($"Das lesen der Userdatei hat leider nicht geklappt! Error: {e.Message}");
-					return false;
-				}
+				Console.WriteLine(File.ReadAllLines(filename));
 			}
 			else
 			{
-				return false;
+				Console.WriteLine("Das hat leider nicht geklappt! Existiert die Datei?");
 			}
+
+			Console.Clear();
+			afterPwd.loggedIn.init();
 		}
-
-		public static string sendMOTD(string? USERNAME)
+		public static void writeMode()
 		{
-			if (USERNAME is null)
+			Console.Write("Dateinamen zum öffnen eingeben: ");
+			string? filename = Console.ReadLine();
+			Console.Write("\nSoll etwas an die Datei ANGEÄNGT werden? [ TRUE | FALSE ] ");
+			string? append = Console.ReadLine();
+
+			bool append_bool = false;
+
+			if (append != null)
 			{
-				return "null";
+				if (append.ToLower() == "true")
+				{
+					append_bool = true;
+				}
+				else if (append.ToLower() == "false")
+				{
+					append_bool = false;
+				}
 			}
 
+			if (File.Exists(filename))
+			{
+				StreamWriter write = new StreamWriter(filename, append_bool);
+
+
+				write.WriteLine(Console.ReadLine());
+
+				write.Close();
+
+			}
+			else
+			{
+				Console.WriteLine("Das hat leider nicht geklappt! Ist die Datei vorhanden und hat keinen Schreibschutz?");
+			}
+
+			Console.Clear();
+			afterPwd.loggedIn.init();
+		}
+	}
+}
+
+public class secManager
+{
+	public void init()
+	{
+		if (!File.Exists("default.pwd"))
+		{
 			try
 			{
-				if (File.Exists(USERNAME + ".pwd"))
+				StreamWriter writer = new StreamWriter("default.pwd");
+				writer.WriteLine("USERNAME=default");
+				writer.WriteLine("PWD=");
+				writer.WriteLine("SETTINGS=true");
+				writer.WriteLine("MOTD=Das ist ein default Account!");
+				writer.Close();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Das erstellen des DeafultUsers hat leider nicht geklappt! Error: {e.Message}");
+			}
+		}
+	}
+
+	public bool chkPwd(string? USERNAME, string? PASSWORD)
+	{
+
+		if (USERNAME is null || PASSWORD is null)
+		{
+			return false;
+		}
+
+
+		if (File.Exists(USERNAME + ".pwd"))
+		{
+			try
+			{
+				bool usernameCorrect = false;       //username check for the current user
+				bool pwdCorrect = false;            //pwd check for the current user
+
+				foreach (string line in File.ReadLines(USERNAME + ".pwd"))
 				{
-					foreach (string line in File.ReadAllLines(USERNAME + ".pwd"))
+					if (line.StartsWith("USERNAME="))
 					{
-						if (line.StartsWith("MOTD="))
+						if (line.Substring(9) == USERNAME)
 						{
-							return line.Substring(5);
+							usernameCorrect = true;
+						}
+						else
+						{
+							usernameCorrect = false;
 						}
 					}
+					else if (line.StartsWith("PWD="))
+					{
+						if (line.Substring(4) == PASSWORD)
+						{
+							pwdCorrect = true;
+						}
+						else
+						{
+							pwdCorrect = false;
+						}
+					}
+				}
 
-					return "null";
-
+				if (usernameCorrect == true && pwdCorrect == true)
+				{
+					return true;
 				}
 				else
 				{
-					return "null";
+					return false;
 				}
 			}
 			catch (Exception e)
 			{
-				return e.Message;
+				Console.WriteLine($"Das lesen der Userdatei hat leider nicht geklappt! Error: {e.Message}");
+				return false;
 			}
 		}
+		else
+		{
+			return false;
+		}
 	}
+
+	public static string sendMOTD(string? USERNAME)
+	{
+		if (USERNAME is null)
+		{
+			return "null";
+		}
+
+		try
+		{
+			if (File.Exists(USERNAME + ".pwd"))
+			{
+				foreach (string line in File.ReadAllLines(USERNAME + ".pwd"))
+				{
+					if (line.StartsWith("MOTD="))
+					{
+						return line.Substring(5);
+					}
+				}
+
+				return "null";
+
+			}
+			else
+			{
+				return "null";
+			}
+		}
+		catch (Exception e)
+		{
+			return e.Message;
+		}
+	}
+}
