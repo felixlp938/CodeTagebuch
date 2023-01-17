ab470ee6eb882b66a87dbbb3fd54dac1f95ab591
Felix <85683853+felixlp938@users.noreply.github.com>
2023-01-17 20:21:40 +0100

    added function to delete user (NOT DONE YET!)
    includet: - if username is null
                    - if inputUser is null
                    - if inputUser is username

--- userManager.cs
+++ userManager.cs
@@ -4,7 +4,36 @@ namespace userManager
 {
     public class deleteUser
     {
-        //implement code here to delete user
+        public static void deleteUserfile(string? USERNAME)
+        {
+            if (USERNAME != null) return;
+
+            Thread.Sleep(100);
+            Console.WriteLine("[Critical ] Benutzer löschen");
+
+            Console.Write("Den Username eingeben: ");
+
+            string? inputUserToDelete = Console.ReadLine();
+
+            if (inputUserToDelete is null)
+            {
+                Console.WriteLine("Der eingegebene Username ist nicht vorhanden!");
+                afterPwd.loggedIn.init();
+            }
+
+            if (inputUserToDelete.ToLower() == USERNAME)
+            {
+                Console.WriteLine("Du kannst dich in deiner eigenen Session nicht löschen!");
+                afterPwd.loggedIn.init();
+            }
+
+            if (File.Exists(inputUserToDelete + ".pwd"))
+            {
+                //delete file with two factor auth here
+            }
+
+
+        }
     }
 
     public class createUser
