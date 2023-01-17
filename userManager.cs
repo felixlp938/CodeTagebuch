de193cb3fb32c3535e7c220c8202d38c3bb651c5
Felix <85683853+felixlp938@users.noreply.github.com>
2023-01-17 20:01:16 +0100

    added classes to delete and create users

---
+++ userManager.cs
@@ -0,0 +1,14 @@
+//(c) 2023, FelixLP; Git: https://github.com/felixlp938
+
+namespace userManager
+{
+    public class deleteUser
+    {
+        //implement code here to delete user
+    }
+
+    public class createUser
+    {
+        //implement code here to create user
+    }
+}
